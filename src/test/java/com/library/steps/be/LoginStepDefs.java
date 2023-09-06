ff9523f2ad131ed7f5ba41d1403e8a86eff7ce0c
AdamB25 <barsialtunit@gmail.com>
2023-09-06 19:36:39 -0400

    LoginStepDefs ready for US1

--- src/test/java/com/library/steps/be/LoginStepDefs.java
+++ src/test/java/com/library/steps/be/LoginStepDefs.java
@@ -1,4 +1,11 @@
 package com.library.steps.be;
 
+import io.cucumber.java.en.Given;
+
 public class LoginStepDefs {
+    @Given("I logged Library api as a {string}")
+    public void i_logged_library_api_as_a(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
 }
